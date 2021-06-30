52763e616eca175308a34c06e86ce52d738694fc
Trevor Makes <the.trevor.makes@gmail.com>
2021-06-29 18:06:56 -0700

    Initial commit

---
+++ include/uCLI.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdint.h>
+
+struct Stream;
+
+namespace uCLI {
+
+// Function pointer to be called when command string is entered
+struct Command {
+  const char* command;
+  void (*callback)(const char*);
+};
+
+// Read string from stream into buffer up to length bytes
+void read_command(Stream& stream, char* buffer, uint8_t length);
+
+// Read string from stream into buffer
+template <typename T, int L>
+void read_command(T& stream, char (&buffer)[L]) {
+    read_command(stream, buffer, L);
+}
+
+// Attempt to match input to list of commands
+void parse_command(Stream& stream, char* input, const Command* commands, uint8_t length);
+
+// Attempt to match input to list of commands
+template <typename T, int L>
+void parse_command(T& stream, char* input, const Command (&commands)[L]) {
+    parse_command(input, commands, L);
+}
+
+// Display prompt and execute command from stream
+template <uint8_t N = 128, typename T, uint8_t L>
+void run_command(T& stream, const Command (&commands)[L]) {
+  char input[N];
+  stream.write('>');
+  read_command(stream, input, N);
+  parse_command(stream, input, commands, L);
+}
+
+} // namespace uCLI
---
+++ src/uCLI.cpp
@@ -0,0 +1,75 @@
+#include "uCLI.hpp"
+#include "uANSI.hpp"
+
+#include <Arduino.h>
+#include <string.h>
+
+namespace uCLI {
+
+void read_command(Stream& stream, char* buffer, uint8_t length) {
+  uint8_t i = 0;
+
+  do {
+    // Get next key press
+    int input = uANSI::read_key(stream);
+    if (input == -1) {
+      continue;
+    }
+
+    // Handle special keys (arrows, etc)
+    if (input == '\e' || input > 0xFF) {
+      continue;
+    }
+
+    // Handle backspace and delete
+    if (input == '\x08' || input == '\x7F') {
+      if (i > 0) {
+        // Delete last character
+        stream.write("\e[D\e[X");
+        --i;
+      }
+      continue;
+    }
+
+    if (input == '\n' || input == '\r') {
+      if (i > 0) {
+        // Exit loop and execute command
+        break;
+      } else {
+        continue;
+      }
+    }
+
+    // Echo and record input
+    stream.write(input);
+    buffer[i++] = input;
+  } while (i < length);
+
+  stream.write("\n");
+  buffer[i] = '\0';
+}
+
+void parse_command(Stream& stream, char* input, const Command commands[], uint8_t length) {
+  char* token = strtok(input, " ");
+
+  // Look for match in command list
+  for (uint8_t i = 0; i < length; ++i) {
+    if (strcmp(token, commands[i].command) == 0) {
+      char* message = strtok(nullptr, "");
+      commands[i].callback(message);
+      return;
+    }
+  }
+
+  // Invalid command; print command list
+  stream.write("Commands: ");
+  for (uint8_t i = 0; i < length; ++i) {
+    if (i > 0) {
+      stream.write(", ");
+    }
+    stream.write(commands[i].command);
+  }
+  stream.write("\n");
+}
+
+} // namespace uCLI
