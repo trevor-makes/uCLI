273521f3787d3dd5b3ce0c70a5a325cc89e85cbb
Trevor Makes <the.trevor.makes@gmail.com>
2022-02-15 22:10:47 -0800

    Remove unnecessary template params

--- include/uCLI.hpp
+++ include/uCLI.hpp
@@ -37,8 +37,8 @@ struct Command {
 void read_command(Stream& stream, char* buffer, uint8_t length, IdleFn idle_fn);
 
 // Read string from stream into buffer
-template <typename T, uint8_t BUF_LEN>
-void read_command(T& stream, char (&buffer)[BUF_LEN], IdleFn idle_fn = nullptr) {
+template <uint8_t BUF_LEN>
+void read_command(Stream& stream, char (&buffer)[BUF_LEN], IdleFn idle_fn = nullptr) {
   read_command(stream, buffer, BUF_LEN, idle_fn);
 }
 
@@ -46,14 +46,14 @@ void read_command(T& stream, char (&buffer)[BUF_LEN], IdleFn idle_fn = nullptr) 
 void parse_command(Stream& stream, char* input, const Command* commands, uint8_t length);
 
 // Attempt to match input to list of commands
-template <typename T, uint8_t CMD_LEN>
-void parse_command(T& stream, char* input, const Command (&commands)[CMD_LEN]) {
+template <uint8_t CMD_LEN>
+void parse_command(Stream& stream, char* input, const Command (&commands)[CMD_LEN]) {
   parse_command(stream, input, commands, CMD_LEN);
 }
 
 // Display prompt and execute command from stream
-template <uint8_t BUF_LEN = 80, typename T, uint8_t CMD_LEN>
-void run_command(T& stream, const Command (&commands)[CMD_LEN], IdleFn idle_fn = nullptr) {
+template <uint8_t BUF_LEN = 80, uint8_t CMD_LEN>
+void run_command(Stream& stream, const Command (&commands)[CMD_LEN], IdleFn idle_fn = nullptr) {
   char input[BUF_LEN];
   stream.write('>');
   read_command(stream, input, BUF_LEN, idle_fn);
