bdc998def0d4693359954cf2e9ff9f5e0afe9c33
Trevor Makes <the.trevor.makes@gmail.com>
2022-02-18 20:07:32 -0800

    Use switch statement in read_command

--- src/uCLI.cpp
+++ src/uCLI.cpp
@@ -23,44 +23,35 @@ void read_command(StreamEx& stream, char* buffer, uint8_t length, IdleFn idle_fn
       continue;
     }
 
-    // Left arrow moves cursor left
-    if (input == uANSI::KEY_LEFT) {
+    switch (input) {
+    case uANSI::KEY_LEFT:
       if (cur > 0) {
         stream.cursor_left();
         --cur;
       }
       continue;
-    }
-
-    // Right arrow moves cursor right
-    if (input == uANSI::KEY_RIGHT) {
+    case uANSI::KEY_RIGHT:
       if (cur < end) {
         stream.cursor_right();
         ++cur;
       }
       continue;
-    }
-
-    // Home moves cursor far left
-    if (input == uANSI::KEY_HOME) {
+    case uANSI::KEY_HOME:
       if (cur > 0) {
+        // Move cursor far left
         stream.cursor_left(cur);
         cur = 0;
       }
       continue;
-    }
-
-    // End moves cursor far right
-    if (input == uANSI::KEY_END) {
+    case uANSI::KEY_END:
       if (cur < end) {
+        // Move cursor far right
         stream.cursor_right(end - cur);
         cur = end;
       }
       continue;
-    }
-
-    // Handle backspace and delete (as backspace)
-    if (input == '\x08' || input == '\x7F') {
+    case '\x08': // ASCII backspace
+    case '\x7F': // ASCII delete (not ANSI delete \e[3~)
       if (cur > 0) {
         stream.cursor_left();
         stream.delete_char();
@@ -72,11 +63,7 @@ void read_command(StreamEx& stream, char* buffer, uint8_t length, IdleFn idle_fn
         --end;
       }
       continue;
-    }
-
-    // Handle enter/return
-    // NOTE uANSI transforms CR and CRLF to LF
-    if (input == '\n') {
+    case '\n': // NOTE uANSI transforms \r and \r\n to \n
       if (end > 0) {
         // Exit loop and execute command
         stream.write('\n');
@@ -86,32 +73,32 @@ void read_command(StreamEx& stream, char* buffer, uint8_t length, IdleFn idle_fn
         // Ignore and continue if line is empty
         continue;
       }
-    }
-
-    // Ignore other non-printable ASCII chars and uANSI input sequences
-    // NOTE UTF-8 multi-byte encodings in [0x80, 0xFF] should be ok
-    if (input < 0x20 || input > 0xFF) {
-      continue;
-    }
-
-    // Prevent typing further characters when buffer is full
-    if (end + 1 == length) {
-      continue;
-    }
-
-    if (cur < end) {
-      // Shift following characters right
-      for (uint8_t i = end; i > cur; --i) {
-        buffer[i] = buffer[i - 1];
+    default:
+      // Ignore other non-printable ASCII chars and uANSI input sequences
+      // NOTE UTF-8 multi-byte encodings in [0x80, 0xFF] should be ok
+      if (input < 0x20 || input > 0xFF) {
+        continue;
       }
-      stream.insert_char();
-    }
 
-    // Echo and record input
-    stream.write(input);
-    buffer[cur] = input;
-    cur++;
-    end++;
+      // Prevent typing further characters when buffer is full
+      if (end + 1 == length) {
+        continue;
+      }
+
+      if (cur < end) {
+        // Shift following characters right
+        for (uint8_t i = end; i > cur; --i) {
+          buffer[i] = buffer[i - 1];
+        }
+        stream.insert_char();
+      }
+
+      // Echo and record input
+      stream.write(input);
+      buffer[cur] = input;
+      cur++;
+      end++;
+    }
   }
 }
 
